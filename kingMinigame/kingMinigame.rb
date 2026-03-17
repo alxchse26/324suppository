ac5b1da112941366775d8bf930d733b7b8f3dd2b
bmthom26 <bmthom26@g.holycross.edu>
2026-03-17 16:56:07 -0400

    Update kingMinigame.rb

--- kingMinigame/kingMinigame.rb
+++ kingMinigame/kingMinigame.rb
@@ -1,6 +1,7 @@
 # Bridget Thomson, Joe Koslosky, Alex Chase
-# emails: , , amchas26@g.holycross.edu
+# emails: bmthom26@g.holycross.edu, , amchas26@g.holycross.edu
 # CSCI 324 -- Programming Languages
 # 21 April 2026
 # Term Final Project
+# Last Modified: 3/17/26
 
